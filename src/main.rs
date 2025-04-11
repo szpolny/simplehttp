1747b0dc542e27cca12ff51c2732aabcfc14513f
Szymon Polny <szymonpolny@outlook.com>
2025-04-11 11:04:06 +0200

    Add connection handling functionality to the HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{net::TcpListener, thread};
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap_or_else(|e| {
@@ -20,4 +24,20 @@ fn main() {
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(e)) => {
+            eprintln!("Failed to read request line: {}", e);
+            return;
+        }
+        None => {
+            eprintln!("No request line found");
+            return;
+        }
+    };
 }
